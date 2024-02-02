88ff6f94dbed06574681629c396d620126d6d576
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2024-02-01 20:01:34 -0500

    Callbacks

--- bin/llamada.dart
+++ bin/llamada.dart
@@ -1,23 +1,37 @@
 import 'package:llamada/llamada.dart' as llamada;
 
 void main() {
-  printMessage(getDay);
-  printMessage(getName);
+  printMessage(
+    (text) => '$text${DateTime.now().weekday}',
+  );
+}
+
+void printMessage(String Function(String text) callback) {
+  print(
+    callback('Fabian'),
+  );
+}
+
+/*
+void main() {
+  printMessage((text) => DateTime.now().weekday.toString());
+  //printMessage(getName);
 
   //Defino una variable inmutable
   //final fn = printMessage;
   //fn(); //Invocamos a la función
 }
 
-void printMessage(String Function() callback) {
+void printMessage(String Function(String text) callback) {
   //se define un parametro que sera una funcion; en este caso voy a  decir que la funcion que yo quiero recibir como parámetro retornará un dato de tipo String
-  print(callback());
+  print(callback('Fanian'));
 }
 
 //definimos dos funciones que van a retornar un String
 
 //Esta primera función lo que va a hacer es retornar el día de la semana como un  string
-String getDay() => DateTime.now().weekday.toString();
+//String getDay() => DateTime.now().weekday.toString();
 
 //Esta segunda función me retorna un nombre
-String getName() => 'Fabian';
+//String getName() => 'Fabian';
+*/
